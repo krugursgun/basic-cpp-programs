b169d901efa61d457c918bca2397f268070f0290
krugursgun <69789431+krugursgun@users.noreply.github.com>
2021-10-09 18:36:14 +0530

    c++ programs
    basics

---
+++ if-else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+        int a,b;
+        cout<<"please enter a number";
+        cin>>a>>b;
+        if(a>b)
+        {
+            cout<<"a is a greater number than b ";
+        }
+
+        else
+        {
+            cout<<"b is a greater number than a ";
+        }
+        return 0;
+    }
